78325e6f50f873b373133ca24cd9e2ebb71f8737
joselopez-moj <jose.lopez@justice.gov.uk>
2026-07-13 11:55:47 +0100

    fix: correct format and add form logic to route

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -19,9 +19,7 @@ class Config(object):
     # test. Set RATELIMIT_ENABLED=false when running the server for those tests.
     RATELIMIT_ENABLED = os.environ.get("RATELIMIT_ENABLED", "true").lower() == "true"
     SECRET_KEY = os.environ.get("SECRET_KEY", "Change me")
-    SERVICE_NAME = os.environ.get(
-        "SERVICE_NAME", "Assess and refer for civil legal advice"
-    )
+    SERVICE_NAME = os.environ.get("SERVICE_NAME", "Assess and refer for civil legal advice")
     SERVICE_PHASE = os.environ.get("SERVICE_PHASE", "Beta")
     SERVICE_URL = os.environ.get("SERVICE_URL", "")
     SESSION_COOKIE_HTTPONLY = True
