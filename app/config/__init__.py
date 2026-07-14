2a199b05bd40bb8f223be7d5e3d7cddae35e2192
Stephanie de Jong <107569008+skdejong@users.noreply.github.com>
2026-07-14 14:01:52 +0100

    Apply ruff formatting

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -15,7 +15,9 @@ class Config(object):
     DEPARTMENT_URL = os.environ.get("DEPARTMENT_URL", "https://mojdigital.blog.gov.uk/")
     RATELIMIT_HEADERS_ENABLED = True
     SECRET_KEY = os.environ.get("SECRET_KEY", "Change me")
-    SERVICE_NAME = os.environ.get("SERVICE_NAME", "Assess and refer for civil legal advice")
+    SERVICE_NAME = os.environ.get(
+        "SERVICE_NAME", "Assess and refer for civil legal advice"
+    )
     SERVICE_PHASE = os.environ.get("SERVICE_PHASE", "Beta")
     SERVICE_URL = os.environ.get("SERVICE_URL", "")
     SESSION_COOKIE_HTTPONLY = True
