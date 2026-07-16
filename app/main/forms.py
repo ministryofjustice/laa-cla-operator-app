c9f4ecc69db554f3ddbe57a178d64ac00671a62c
charles <chalston.pinnock@justice.gov.uk>
2026-07-16 10:20:52 +0100

    adding all the latest changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,29 +1,14 @@
-from flask_wtf import FlaskForm
-from govuk_frontend_wtf.wtforms_widgets import GovSubmitInput
+from govuk_frontend_wtf.wtforms_widgets import GovRadioInput, GovSubmitInput
+from app.main.utils.widgets import CustomRadioInput
 from wtforms.fields import RadioField, SubmitField
 from wtforms.validators import InputRequired
-
-from app.main.utils.widgets import CustomRadioInput
-
-
-class WhosCallingForm(FlaskForm):
-    whosCalling = RadioField(
-        "Are you calling on behalf of yourself or another person?",
-        widget=CustomRadioInput(heading_class="govuk-fieldset__legend--s"),
-        validators=[
-            InputRequired(
-                message="You must select either 'Myself' or 'Another person'"
-            )
-        ],
-        choices=[("myself", "Myself"), ("another", "Another person")],
-    )
-    submit = SubmitField("Continue", widget=GovSubmitInput())
+from flask_wtf import FlaskForm
 
 
 class CookiesForm(FlaskForm):
     functional = RadioField(
         "Do you want to accept functional cookies?",
-        widget=CustomRadioInput(heading_class="govuk-fieldset__legend--m"),
+        widget=GovRadioInput(),
         validators=[
             InputRequired(message="Select yes if you want to accept functional cookies")
         ],
@@ -32,7 +17,7 @@ class CookiesForm(FlaskForm):
     )
     analytics = RadioField(
         "Do you want to accept analytics cookies?",
-        widget=CustomRadioInput(heading_class="govuk-fieldset__legend--m"),
+        widget=GovRadioInput(),
         validators=[
             InputRequired(message="Select yes if you want to accept analytics cookies")
         ],
@@ -40,3 +25,15 @@ class CookiesForm(FlaskForm):
         default="no",
     )
     save = SubmitField("Save cookie settings", widget=GovSubmitInput())
+
+
+class WhosCallingForm(FlaskForm):
+    whos_calling = RadioField(
+        "Are you calling on behalf of yourself or another person?",
+        widget=CustomRadioInput(heading_class="govuk-fieldset__legend--s"),
+        validators=[
+            InputRequired(message="You must select either 'Myself' or 'Another person'")
+        ],
+        choices=[("myself", "Myself"), ("another", "Another person")],
+    )
+    submit = SubmitField("Continue", widget=GovSubmitInput())
