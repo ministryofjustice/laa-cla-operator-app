3dd3cd873330a8fdf174ad82423e3da9f8af7577
charles <chalston.pinnock@justice.gov.uk>
2026-08-25 17:54:23 +0100

    adding message about failed login

--- app/authenication/entra.py
+++ app/authenication/entra.py
@@ -158,7 +158,7 @@ class EntraLogin:
             if user:
                 return redirect(url_for("search_client"))
             else:
-                flash(f"Error in try to login {e}", "error")
+                flash(f"Login failed: {e}", "error")
                 return render_template("auth/sign_in.html")
 
         return render_template("auth/sign_in.html")
