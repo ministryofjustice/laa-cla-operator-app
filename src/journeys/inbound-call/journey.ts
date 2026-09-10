18ef87378668d9f27c73fd7a1f7c1b04cc64f41b
Stephanie de Jong <107569008+skdejong@users.noreply.github.com>
2026-09-10 17:26:50 +0100

    Add fields and styling to add-client-details step

--- src/journeys/inbound-call/journey.ts
+++ src/journeys/inbound-call/journey.ts
@@ -7,6 +7,9 @@ import {
 import {
   GovUKButton,
   GovUKRadioInput,
+  GovUKTextInput,
+  GovUKDateInputFull,
+  GovUKHeading,
 } from "@ministryofjustice/hmpps-forge/govuk-components";
 
 // Step 1: Who's calling
@@ -65,14 +68,104 @@ const searchClient = step({
   ],
 });
 
-// Step 3: Add new client if click on "Start a new case"
+// Step 3: Add new client if clicked on "Start a new case"
 const addClientDetailsStep = step({
   code: "add-client-details",
   path: "/add-client-details",
   title: "Add new client",
   reachability: { entryWhen: true },
   view: { template: "main/add-client-details.njk" },
-  blocks: [GovUKButton({ text: "Save and continue" })],
+  blocks: [
+    GovUKHeading({ text: "Client's details", size: "m" }),
+
+    GovUKTextInput({
+      code: "fullName",
+      label: { text: "Name", classes: "govuk-label--s" },
+    }),
+
+    GovUKDateInputFull({
+      code: "dateOfBirth",
+      fieldset: {
+        legend: {
+          text: "What's your date of birth?",
+          classes: "govuk-fieldset__legend--s",
+        },
+      },
+      hint: { text: "For example, 27 3 2007" },
+    }),
+
+    GovUKHeading({ text: "Client's contact details", size: "m" }),
+
+    GovUKTextInput({
+      code: "phoneNumber",
+      label: { text: "Phone number", classes: "govuk-label--s" },
+    }),
+
+    GovUKRadioInput({
+      code: "safeToCall",
+      fieldset: {
+        legend: {
+          text: "Is it safe to call this number?",
+          classes: "govuk-fieldset__legend--s",
+        },
+      },
+      items: [
+        { value: "yes", text: "Yes" },
+        { value: "no", text: "No" },
+      ],
+    }),
+
+    GovUKRadioInput({
+      code: "safeToLeaveMessage",
+      fieldset: {
+        legend: {
+          text: "Is it safe to leave a message?",
+          classes: "govuk-fieldset__legend--s",
+        },
+      },
+      items: [
+        { value: "yes", text: "Yes" },
+        { value: "no", text: "No" },
+      ],
+    }),
+
+    GovUKRadioInput({
+      code: "acceptsWithheldCalls",
+      fieldset: {
+        legend: {
+          text: "If safe to call, does your phone accept calls from a withheld number?",
+          classes: "govuk-fieldset__legend--s",
+        },
+      },
+      hint: {
+        text: "When we call you back, our number will not display and your phone may show 'No Caller ID', 'Private number' or 'Number withheld'. Call screening settings mean some phones do not accept withheld numbers.",
+      },
+      items: [
+        {
+          value: "yes",
+          text: "Yes",
+          hint: {
+            text: "Prompt the client to add Civil Legal Advice as a contact using 0345 345 4345 'just in case'",
+          },
+        },
+        {
+          value: "no",
+          text: "No",
+          hint: {
+            text: "We will try to call you back 3 times but if we can't get through you will have to contact us again to continue your case. Prompt the client to adjust the settings on their phone or add Civil Legal Advice as a contact using 0345 345 4345.",
+          },
+        },
+      ],
+    }),
+
+    GovUKTextInput({
+      code: "email",
+      label: { text: "Email (optional)", classes: "govuk-label--s" },
+      inputType: "email",
+    }),
+
+    GovUKButton({ text: "Save and continue" }),
+  ],
   onSubmission: [
     submit({
       validate: false,
