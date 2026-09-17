91eb1276163d128fab27ede3c3331155cd0435ef
charles <chalston.pinnock@justice.gov.uk>
2026-09-17 17:01:48 +0100

    saving the stages

--- src/journeys/inbound-call/blocks/addAddressBlock.ts
+++ src/journeys/inbound-call/blocks/addAddressBlock.ts
@@ -1,10 +1,32 @@
 import { Condition, Self, validation } from "@ministryofjustice/hmpps-forge/core/authoring";
 import { CollectionBlock } from "@ministryofjustice/hmpps-forge/core/components"
-import { GovUKButton, GovUKRadioInput } from "@ministryofjustice/hmpps-forge/govuk-components"
+import { GovUKButton, GovUKTextInput } from "@ministryofjustice/hmpps-forge/govuk-components"
 
 export const addAddress = CollectionBlock({
     collection: [
+            GovUKTextInput(
+                { code: 'Address line 1 ',
+                 label: { text: 'Address line 1' } }
+                ),
+            GovUKTextInput(
+                { code: 'Address line 2 (optional) ',
+                     label: 
+                     { text: 'Address line 2 (optional) ' } 
+                    }),
 
-            GovUKButton({ text: "Continue" })
+            GovUKTextInput(
+                { code: 'Town or city ',
+                     label: 
+                     { text: 'Town or city' } }
+                ),
+
+            GovUKTextInput(
+                { code: 'Postcode ',
+                     label: { text: 'Postcode' } 
+                    }),
+
+            GovUKButton(
+                { text: "Continue" }
+            )
     ]
 });
