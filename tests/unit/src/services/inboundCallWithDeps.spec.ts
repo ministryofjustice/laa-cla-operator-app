2dce10ee8b61bd80f1469442a378db1be5c9ec71
said-moj <45761276+said-moj@users.noreply.github.com>
2026-09-18 10:26:15 +0100

    Fix failing test

--- tests/unit/src/services/inboundCallWithDeps.spec.ts
+++ tests/unit/src/services/inboundCallWithDeps.spec.ts
@@ -1,68 +1,75 @@
-// import { strict as assert } from 'assert';
-// import sinon from 'sinon';
-// import { InboundCallEffectsImplementation } from '#src/journeys/effects.js';
-// import type { AxiosInstanceWrapper } from '#types/axios-instance-wrapper.js';
+import { strict as assert } from 'assert';
+import sinon from 'sinon';
+import { InboundCallEffectsImplementation } from '#src/journeys/effects.js';
+import type { AxiosInstanceWrapper } from '#types/axios-instance-wrapper.js';
 
-// describe('InboundCallEffectsImplementation.GetAllCases', () => {
-//   function makeAxiosWrapper(): AxiosInstanceWrapper {
-//     const get = sinon.stub();
+async function mokcPostcodeLookupService(building: string, postcode: string): Promise<{ address: string; }[] | null> {
+  return null
+}
 
-//     return {
-//       axiosInstance: {
-//         defaults: {
-//           headers: { common: {} },
-//         },
-//       },
-//       get,
-//       delete: sinon.stub(),
-//       head: sinon.stub(),
-//       options: sinon.stub(),
-//       post: sinon.stub(),
-//       put: sinon.stub(),
-//       patch: sinon.stub(),
-//       request: sinon.stub(),
-//       use: sinon.stub(),
-//     } as unknown as AxiosInstanceWrapper;
-//   }
 
-//   it('sets allCases data when context has authenticatedAxios wrapper', async () => {
-//     const axiosWrapper = makeAxiosWrapper();
-//     const expected = { count: 1, results: [{ reference: 'FA-1' }] };
-//     const getAllCases = sinon.stub().resolves(expected);
+describe('InboundCallEffectsImplementation.GetAllCases', () => {
+  function makeAxiosWrapper(): AxiosInstanceWrapper {
+    const get = sinon.stub();
 
-//     const effect = InboundCallEffectsImplementation.GetAllCases({
-//       caseApi: { getAllCases },
-//     });
+    return {
+      axiosInstance: {
+        defaults: {
+          headers: { common: {} },
+        },
+      },
+      get,
+      delete: sinon.stub(),
+      head: sinon.stub(),
+      options: sinon.stub(),
+      post: sinon.stub(),
+      put: sinon.stub(),
+      patch: sinon.stub(),
+      request: sinon.stub(),
+      use: sinon.stub(),
+    } as unknown as AxiosInstanceWrapper;
+  }
 
-//     const setData = sinon.stub();
-//     const context = {
-//       getState: sinon.stub().withArgs('authenticatedAxios').returns(axiosWrapper),
-//       setData,
-//     };
+  it('sets allCases data when context has authenticatedAxios wrapper', async () => {
+    const axiosWrapper = makeAxiosWrapper();
+    const expected = { count: 1, results: [{ reference: 'FA-1' }] };
+    const getAllCases = sinon.stub().resolves(expected);
 
-//     await effect(context as any);
+    const effect = InboundCallEffectsImplementation.GetAllCases({
+      caseApi: { getAllCases },
+      postcodeapi: {lookup: mokcPostcodeLookupService}
+    });
 
-//     assert.equal(getAllCases.calledOnceWithExactly(axiosWrapper), true);
-//     assert.equal(setData.calledOnceWithExactly('allCases', expected), true);
-//   });
+    const setData = sinon.stub();
+    const context = {
+      getState: sinon.stub().withArgs('authenticatedAxios').returns(axiosWrapper),
+      setData,
+    };
 
-//   it('throws when authenticatedAxios is missing or invalid', async () => {
-//     const effect = InboundCallEffectsImplementation.GetAllCases({
-//       caseApi: { getAllCases: sinon.stub() },
-//     });
+    await effect(context as any);
 
-//     const context = {
-//       getState: sinon.stub().withArgs('authenticatedAxios').returns(undefined),
-//       setData: sinon.stub(),
-//     };
+    assert.equal(getAllCases.calledOnceWithExactly(axiosWrapper), true);
+    assert.equal(setData.calledOnceWithExactly('allCases', expected), true);
+  });
 
-//     await assert.rejects(
-//       () => effect(context as any),
-//       (error: unknown) => {
-//         assert(error instanceof Error);
-//         assert.equal(error.message, 'Axios middleware is not available in the context.');
-//         return true;
-//       }
-//     );
-//   });
-// });
+  it('throws when authenticatedAxios is missing or invalid', async () => {
+    const effect = InboundCallEffectsImplementation.GetAllCases({
+      caseApi: { getAllCases: sinon.stub() },
+      postcodeapi: {lookup: mokcPostcodeLookupService},
+    });
+
+    const context = {
+      getState: sinon.stub().withArgs('authenticatedAxios').returns(undefined),
+      setData: sinon.stub(),
+    };
+
+    await assert.rejects(
+      () => effect(context as any),
+      (error: unknown) => {
+        assert(error instanceof Error);
+        assert.equal(error.message, 'Axios middleware is not available in the context.');
+        return true;
+      }
+    );
+  });
+});
