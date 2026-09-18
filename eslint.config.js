a7a7796e85984a974f4e1f33e5904b2e003ef1a7
Will Clarke <william.clarke@digital.justice.gov.uk>
2026-09-18 13:47:17 +0100

    ensure local lint matches the CI linting

--- eslint.config.js
+++ eslint.config.js
@@ -113,7 +113,9 @@ export default [
       'docs/source/javascripts/govuk_frontend.js', // Documentation JavaScript file, not part of main TypeScript project
       'eslint.config.js', // Parsing error this file was not found by the project service. Consider either including it in the `tsconfig.json` or including it in `allowDefaultProject`,
       'coverage', // Ignore the code coverage output from linter
-      'scripts/e2e_coverage/*' // Route coverage analysis scripts
+        'scripts/e2e_coverage/*', // Route coverage analysis scripts
+        '.venv/*', // Ignore local Python environments
+        'playwright/*' // Ignore local Playwright output
     ]
   },
 ];
