77b02ab31e4095b030ad4d723ad865941c61a575
said-moj <45761276+said-moj@users.noreply.github.com>
2026-09-21 10:18:41 +0100

    Disable @typescript-eslint/prefer-destructuring

--- src/app.ts
+++ src/app.ts
@@ -111,7 +111,8 @@ const createApp = (): express.Application => {
 	for (const journeyPackage of journeyPackages) {
 		const steps = journeyPackage.journey.steps ?? []
 		const journeyPath = journeyPackage.journey.path.replace(/^\/+|\/+$/g, "");
-		pathLookup[journeyPackage.journey.code] = journeyPackage.journey.path;
+		// disable @typescript-eslint/prefer-destructuring - Don't want to introduce `code` and `path` variables here as their use is ambiguous at this point.
+		pathLookup[journeyPackage.journey.code] = journeyPackage.journey.path; // eslint-disable-line @typescript-eslint/prefer-destructuring
 		for(const step of  steps) {
 			const code = `${journeyPackage.journey.code}.${step.code}`
 			const stepPath = step.path.replace(/^\/+|\/+$/g, "");
