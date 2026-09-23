6e127c580de83d268e5f79411ecfadc1da365887
charles <chalston.pinnock@justice.gov.uk>
2026-09-23 12:43:16 +0100

    updating the page for new address

--- src/journeys/inbound-call/blocks/addAddressBlock.ts
+++ src/journeys/inbound-call/blocks/addAddressBlock.ts
@@ -1,6 +1,6 @@
 import { Condition, Self, validation } from "@ministryofjustice/hmpps-forge/core/authoring";
 import { CollectionBlock } from "@ministryofjustice/hmpps-forge/core/components"
-import { GovUKButton, GovUKTextInput,GovUKTextareaInput } from "@ministryofjustice/hmpps-forge/govuk-components"
+import { GovUKButton, GovUKTextInput, GovUKTextareaInput } from "@ministryofjustice/hmpps-forge/govuk-components"
 
 export const addAddress = CollectionBlock({
     collection: [
@@ -12,56 +12,16 @@ export const addAddress = CollectionBlock({
             validWhen: [
                 validation({
                     condition: Self().match(Condition.IsRequired()),
-                    message: "Enter address line 1"
-                }),
-            ],
+                    message: "Address field cannot be blank" 
+                })
+            ]
         }),
-
-        GovUKTextInput({
-            code: 'address-line-2',
-            label: { text: 'Address line 2 (optional)' },
-            classes: 'govuk-input--width-20'
-        }),
-
-        GovUKTextInput({
-            code: 'town-or-city',
-            label: { text: 'Town or city' },
-            classes: 'govuk-input--width-20',
-            validWhen: [
-                validation({
-                    condition: Self().match(Condition.IsRequired()),
-                    message: "Enter a town or city"
-                }),
-            ],
-        }),
-
-        GovUKTextInput({
-            code: 'country',
-            label: { text: 'Country' },
-            classes: 'govuk-input--width-20',
-            validWhen: [
-                validation({
-                    condition: Self().match(Condition.IsRequired()),
-                    message: "Enter a country"
-                }),
-            ],
-        }),
-
+        
         GovUKTextInput({
         code: 'postcode',
-        label: { text: 'Postcode' },
+        label: { text: 'Postcode (Optional)' },
         classes: 'govuk-input--width-10',
         autocomplete: 'postal-code',
-        validWhen: [
-            validation({
-                condition: Self().match(Condition.IsRequired()),
-                message: "Enter a postcode"
-            }),
-            validation({
-                condition: Self().match(Condition.Address.IsValidPostcode()),
-                message: "Enter a full UK postcode, like AA1 1AA"
-            }),
-        ],
     }),
         GovUKButton({ text: "Use this address" })
     ]
