b987f29e2332e53ae7eb26f82a565dc25c5e1bc2
said-moj <45761276+said-moj@users.noreply.github.com>
2026-09-24 12:47:38 +0100

    Remove console.logs from callback

--- src/controllers/silasController.ts
+++ src/controllers/silasController.ts
@@ -299,18 +299,13 @@ export async function callbackAction(req: Request, res: Response): Promise<void>
 
   const code = typeof req.query.code === "string" ? req.query.code : "";
   const state = typeof req.query.state === "string" ? req.query.state : "";
-  console.log(`Code is ${code}`)
-  console.log(`State is ${state}`)
-  console.log(`Session nonce is ${req.session.auth_nonce}`)
 
   if (code.length === EMPTY_LENGTH || state.length === EMPTY_LENGTH) {
-    console.log("Code or state is empty")
     sendAuthenticationFailure(res);
     return;
   }
 
   if (state !== req.session.auth_nonce) {
-    console.log("State and nonce are not equal")
     sendAuthenticationFailure(res);
     return;
   }
@@ -323,7 +318,6 @@ export async function callbackAction(req: Request, res: Response): Promise<void>
     });
 
     if (!hasValidAccountResponse(response)) {
-      console.log("NON SUCCESS RETURN FROM acquireTokenByCode ", response)
       sendAuthenticationFailure(res);
       return;
     }
